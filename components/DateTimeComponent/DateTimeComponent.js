251751b15e829591be265d575079e72f33402502
bilal1031 <bilalnaeem166@gmail.com>
2022-05-20 01:25:22 +0500

    time picker issue fixed

--- components/DateTimeComponent/DateTimeComponent.js
+++ components/DateTimeComponent/DateTimeComponent.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { View, Platform, StyleSheet } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import CalendarPicker from "react-native-calendar-picker/CalendarPicker";
 import moment from "moment";
 
 import { colors } from "../../utilities/colors";
-import CalendarPicker from "react-native-calendar-picker/CalendarPicker";
 import TealButton from "../TealButton/TealButton";
 import Selectable from "../Selectable/Selectable";
+import ColoredText from "../ColoredText/ColoredText";
 
 export default function DateTimeComponent({
   selectedItem,
@@ -16,25 +17,21 @@ export default function DateTimeComponent({
   const [date, setDate] = useState(
     moment().format("dddd, MMMM Do, YYYY").toString()
   );
-  const [time, setTime] = useState("Set Time");
+  const [time, setTime] = useState(new Date());
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
 
-  const showTimePicker = () => {
-    setTimePickerVisibility(true);
-  };
-
   return (
-    <View style={{ alignItems: "center", padding: 20 }}>
+    <View style={styles.componentContainer}>
       <Selectable
         bgColor={colors.teal100}
         isActive={true}
-        style={{ alignItems: "center", height: 40 }}
+        style={styles.dateSelectableContainer}
         contentStyle={{ height: 40 }}
         icon={null}
       >
         {date}
       </Selectable>
-      <View>
+      <View style={styles.dateTimeComponentContainer}>
         <CalendarPicker
           onDateChange={(value) => {
             setDate(moment(value).format("dddd, MMMM Do, YYYY").toString());
@@ -43,50 +40,66 @@ export default function DateTimeComponent({
           width={300}
         />
 
-        <Selectable
-          bgColor={colors.teal100}
-          isActive={true}
-          style={styles.selectable}
-          contentStyle={{ height: 40 }}
-          icon={null}
-          onPress={() => setTimePickerVisibility(true)}
+        <ColoredText
+          color={colors.teal100}
+          style={{ alignSelf: "center", marginTop: 25 }}
         >
-          {time}
-        </Selectable>
-        <>
-          {Platform.OS === "ios" ? (
+          Set Time
+        </ColoredText>
+        {Platform.OS === "ios" ? (
+          <>
+            <DateTimePicker
+              mode="time"
+              style={{
+                alignSelf: "center",
+                width: 80,
+                marginTop: 5,
+              }}
+              value={time}
+              onChange={(event, newTime) => {
+                setTime(newTime);
+              }}
+            />
+          </>
+        ) : (
+          <Selectable
+            bgColor={colors.teal100}
+            isActive={true}
+            style={styles.selectable}
+            contentStyle={{ height: 40 }}
+            icon={null}
+            onPress={() => setTimePickerVisibility(true)}
+          >
+            {moment(time).format("hh:mm A").toString()}
+          </Selectable>
+        )}
+        {isTimePickerVisible && (
+          <>
             <DateTimePicker
               mode="time"
               style={{ flex: 1 }}
-              value={new Date()}
+              value={time}
               onChange={(event, newTime) => {
-                setTime(moment(newTime).format("hh:mm A"));
+                setTimePickerVisibility(false);
+                setTime(newTime);
               }}
             />
-          ) : (
-            isTimePickerVisible && (
-              <>
-                <DateTimePicker
-                  mode="time"
-                  style={{ flex: 1 }}
-                  value={new Date()}
-                  onChange={(event, newTime) => {
-                    setTime(moment(newTime).format("hh:mm A"));
-                    setTimePickerVisibility(false);
-                  }}
-                  t
-                />
-              </>
-            )
-          )}
-        </>
+          </>
+        )}
       </View>
+
       <View style={{ marginTop: 25 }}>
         <TealButton
           text="Set Deadline"
           onPress={() => {
-            setSelectedItem(`${date} at ${time}`);
-            closeScreen();
+            if (time != "Set Time") {
+              setSelectedItem(
+                `${date} at ${moment(time).format("hh:mm A").toString()}`
+              );
+              closeScreen();
+            } else {
+              alert("Set Time");
+            }
           }}
         />
       </View>
@@ -95,11 +108,20 @@ export default function DateTimeComponent({
 }
 
 const styles = StyleSheet.create({
+  componentContainer: { alignItems: "center", padding: 20 },
   selectable: {
     alignItems: "center",
     height: 40,
     marginTop: 25,
     width: 250,
     alignSelf: "center",
+  },
+  dateSelectableContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 40,
+  },
+  dateTimeComponentContainer: {
+    // justifyContent: "center",
   },
 });
